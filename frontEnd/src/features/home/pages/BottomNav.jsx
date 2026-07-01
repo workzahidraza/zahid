9d3b78101c92a2a74d206a3c51a27c845d43525b
Shaikh Zahid Raza <work.zahidraza@gmail.com>
2026-07-02 00:43:30 +0530

    feat: enhance BottomNav component with post creation form toggle and styling

--- frontEnd/src/features/home/pages/BottomNav.jsx
+++ frontEnd/src/features/home/pages/BottomNav.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 const BottomNav = () => {
+  const [cretePost, setCreatepost] = useState(false);
   return (
     <>
       <div
-        className="post-container w-[95vw] bg-white/10 backdrop-blur-md h-[50vh] rounded-2xl fixed top-[70%] left-[50%] -translate-x-1/2
-    -translate-y-1/2 px-2 py-1"
+        className={`post-container w-[95vw] bg-white/10 backdrop-blur-md h-[50vh] rounded-2xl fixed top-[70%] left-[50%] -translate-x-1/2
+    -translate-y-1/2 px-2 py-1 ${cretePost ? "block" : "hidden"}`}
       >
         <div className="formBox  h-full w-full flex justify-center items-center ">
-          <form className="form bg-red-400 h-[40vh] w-[70vw] px-2 py-1 ">asda</form>
+          <form className="form h-full w-full px-2 py-1 flex flex-col justify-center items-center gap-3 ">
+            <label
+              htmlFor="photo"
+              className="h-32 w-32 rounded-full bg-[#2E3532] flex items-center justify-center cursor-pointer hover:bg-gray-600"
+            >
+              <i className="ri-image-upload-line text-4xl text-white"></i>
+            </label>
+            <input
+              className="hidden "
+              type="file"
+              placeholder="upload pics"
+              id="photo"
+              name="photo"
+            />
+            <input
+              className="bg-amber-50 w-full mt-2 px-2 y-1 h-10 rounded-lg outline-none border-none"
+              type="text"
+              placeholder="enter caption"
+            />
+            <button className="bg-[#2E3532] text-[#8B2635] text-2xl font-bold w-full mt-2 px-2 y-1 h-10 rounded-lg outline-none border-none">
+              upload
+            </button>
+          </form>
         </div>
       </div>
 
@@ -20,9 +43,13 @@ const BottomNav = () => {
           }}
         ></i>
         <i
-          class="ri-add-large-fill bg-[#8B2635] w-18 h-18 rounded-full flex justify-center items-center absolute left-1/2 -translate-x-1/2 -translate-y-4"
+          class={`ri-add-large-fill bg-[#8B2635] w-18 h-18 rounded-full flex justify-center items-center absolute left-1/2 -translate-x-1/2 -translate-y-4 `}
           onClick={() => {
-            <Link to="/createPost"></Link>;
+            if (cretePost == false) {
+              setCreatepost(true);
+            } else {
+              setCreatepost(false);
+            }
           }}
         ></i>
 
